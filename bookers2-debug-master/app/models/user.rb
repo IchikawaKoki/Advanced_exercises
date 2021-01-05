215434646f6ca5d3a6e6b31170315833452ad4c1
IchikawaKoki <k.kou.1kw5.13@icloud.com>
2021-01-05 16:02:29 +0000

    [Finish]応用課題4

--- bookers2-debug-master/app/models/user.rb
+++ bookers2-debug-master/app/models/user.rb
@@ -8,8 +8,26 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+  def follow(user)
+    active_relationships.create(followed_id: user)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user).destroy
+  end
 end
