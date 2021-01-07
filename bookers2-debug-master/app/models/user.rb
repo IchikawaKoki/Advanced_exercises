171b03c83ae50203dfc7760fa5fc83466c92664d
IchikawaKoki <k.kou.1kw5.13@icloud.com>
2021-01-07 08:27:01 +0000

    [Finish]応用課題7

--- bookers2-debug-master/app/models/user.rb
+++ bookers2-debug-master/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include JpPrefecture
+  jp_prefecture :prefecture_code
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -42,5 +44,13 @@ class User < ApplicationRecord
       User.where('name LIKE ?', "%#{word}%")
     end
   end
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+end
 
 end
